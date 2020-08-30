c0acf7969bf51fd3aab002413d0a7f0c842a2b72
jasurn <97jasur@gmail.com>
2020-08-30 23:11:12 +0500

    Changed page title

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+// @ts-ignore
+import UnderConstruction from 'react-under-construction';
+import 'react-under-construction/build/css/index.css';
+import github from './logo/github2.svg';
+
+function App() {
+    return (
+        <UnderConstruction
+            background={{
+                image: 'https://static.pexels.com/photos/259698/pexels-photo-259698.jpeg',
+                textColor: '#fff',
+                overlay: {
+                    color: '#000',
+                    opacity: '.5'
+                }
+            }}
+            logo={{
+                src: 'https://image.ibb.co/b7guP5/Rubbby_without_text.png',
+                alt: 'alt text'
+            }}
+            title={{
+                text: 'jnabijonov'
+            }}
+            description={{
+                text: 'Website is under construction. We\'ll be here soon',
+                style: {
+                    maxWidth: '440px',
+                }
+            }}
+
+            links={[
+                {
+                    url: 'https://github.com/jasurn',
+                    image: github,
+                },
+                {
+                    url: 'https://www.linkedin.com/in/jasurn/',
+                    image: 'https://image.flaticon.com/icons/svg/145/145807.svg',
+                },
+                {
+                    url: 'mailto:97jasur@gmail.com',
+                    image: 'https://image.flaticon.com/icons/svg/321/321817.svg',
+                },
+            ]}
+        />
+    );
+}
+
+export default App;
