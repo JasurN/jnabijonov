8477b877f891fb26fa4d7c48d5361eccef5c678b
jasurn <97jasur@gmail.com>
2020-08-31 16:26:40 +0500

    Updates

---
+++ precache-manifest.b64365090550881f18220391ac6c85d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d466a90008d769e8676b1b81897f2bb7",
+    "url": "/jnabijonov/index.html"
+  },
+  {
+    "revision": "f6961f35f693d6ccaabc",
+    "url": "/jnabijonov/static/css/2.2e9c039f.chunk.css"
+  },
+  {
+    "revision": "f6961f35f693d6ccaabc",
+    "url": "/jnabijonov/static/js/2.4031fd54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jnabijonov/static/js/2.4031fd54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ce3c4277e1ea030e41",
+    "url": "/jnabijonov/static/js/main.73d6d5da.chunk.js"
+  },
+  {
+    "revision": "01f9cd9e70cbac8feb4d",
+    "url": "/jnabijonov/static/js/runtime-main.c2176c2a.js"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/jnabijonov/static/media/github.192a6620.svg"
+  }
+]);
